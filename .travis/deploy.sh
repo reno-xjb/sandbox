9e9101e5330bceb4a168e965e849784d78416b7f
reno-xjb <23520255+reno-xjb@users.noreply.github.com>
2019-07-23 22:01:13 +0200

    ci: update travis configuration

--- .travis/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-git config user.email "travis@travis-ci.org"
-git config user.name "Travis CI"
-git remote set-url origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
-git checkout master
-
-yarn lerna:version --yes
