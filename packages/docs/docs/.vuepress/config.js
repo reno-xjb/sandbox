0375c364fedc893639779d6387e074b460897905
reno-xjb <23520255+reno-xjb@users.noreply.github.com>
2019-08-26 16:51:48 +0200

    docs: add docs

---
+++ packages/docs/docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  base: '/sandbox/',
+  title: 'reno-sandbox',
+  description: 'Just a repo to test tools',
+  evergreen: true,
+  themeConfig: {
+    repo: 'reno-xjb/sandbox',
+    repoLabel: 'Contribute',
+    docsDir: 'docs',
+    editLinks: true,
+    editLinkText: 'Help us improve this page!',
+  },
+}
---
+++ packages/docs/docs/index.md
@@ -0,0 +1,8 @@
+---
+home: true
+heroText: reno-sandbox
+tagline: Just a repo to test tools
+actionText: Go to repo →
+actionLink: https://github.com/reno-xjb/sandbox
+footer: MIT Licensed | Copyright © 2019-present reno-xjb
+---
